8790afeaec1c8d9c5895951f5ab4985656485be7
jackmmd <ivanmondragonmanchay@gmail.com>
2025-06-08 21:52:06 -0500

    ✨ Styles: new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react"
 import hamster from './assets/hamster.jpg'
+import patitaRoja from './assets/patita-roja.png'
+import patitaRosa from './assets/patita-rosada.png'
+import perrpAsao from './assets/asao.webp'
 export default function App() {
   const [response, setResponse] = useState(true)
   const [showText, setShowText] = useState(false)
+  const [showAsao, setShowAsao] = useState(false)
   const [top, setTop] = useState(0)
-
+  function hiddenNo() {
+    if (top === 52) {
+      setTop(0)
+    } else {
+      setTop(52)
+    }
+    setShowAsao(true)
+  }
   return (
     <section className="border h-screen flex flex-col justify-between">
       <div>
-
         <h1 className="text-center mt-10 text-xl font-bold">¿Quieres ser mi novia?</h1>
         {showText &&
           <div>
@@ -20,25 +30,40 @@ export default function App() {
             <p className="text-center">Sabía que ibas a decir que si</p>
           </div>
         }
+        {showAsao &&
+          <div >
+            <img
+              src={perrpAsao}
+              alt=""
+              width={150}
+              className="mx-auto mt-2" />
+            <p className="text-center font-semibold text-red-500">No me rechazes</p>
+          </div>
+        }
         <div className="flex justify-center">
           <div className="mt-10 flex gap-3 absolute">
             <button
+              onMouseOver={() => {
+                setShowAsao(false)
+              }}
               onClick={() => {
                 setShowText(true)
                 setResponse(true)
               }}
-              className="bg-green-600 px-4 rounded-md cursor-pointer h-10 w-16">Si</button>
+              className="border border-pink-600 hover:bg-pink-200 px-4 rounded-md cursor-pointer h-10 w-21 flex items-center gap-1">
+              <img src={patitaRosa} alt="" height={25} width={25} />
+              <p>Si</p>
+            </button>
             {response === false ? null :
               <button
-                onClick={() => setResponse(false)}
-                onMouseOver={() => {
-                  if (top === 52) {
-                    setTop(0)
-                  } else {
-                    setTop(52)
-                  }
-                }}
-                className={`bg-red-600 px-4 rounded-md cursor-pointer h-10 w-16 relative top-${String(top)}`}>No</button>
+                onClick={hiddenNo}
+                onMouseOver={hiddenNo}
+                className={`border border-red-500 px-4 rounded-md cursor-pointer h-10 w-22 relative top-${String(top)} flex items-center justify-between`}>
+                <img src={patitaRoja} alt="" height={25} width={25} />
+                <p className="ml-1">
+                  No
+                </p>
+              </button>
             }
           </div>
         </div>
